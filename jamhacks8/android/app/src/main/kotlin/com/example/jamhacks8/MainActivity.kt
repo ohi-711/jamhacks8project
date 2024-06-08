4b06a2ec6ff5dbf77b913fd914f1a3e34e3887df
April <92280895+ohi-711@users.noreply.github.com>
2024-06-08 01:14:49 -0400

    Delete jamhacks8 directory

--- jamhacks8/android/app/src/main/kotlin/com/example/jamhacks8/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jamhacks8
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- jamhacks8/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:jamhacks8/app/smoge_app.dart';
-
-void main() => runApp(SmogeApp());
